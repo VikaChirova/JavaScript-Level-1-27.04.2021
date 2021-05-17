0460d8dd3ca957ba0a66536b6d2d92158e59c592
VikaChirova <80957809+VikaChirova@users.noreply.github.com>
2021-05-17 19:32:56 +0300

    edit file

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,25 +1,3 @@
-
-var table = document.createElement("table");
-var thead = document.createElement("thead");
-var tr = document.createElement("tr");
-var tfoot = document.createElement("tfoot");
-var td = document.createElement("td");
-var th = document.createElement("th");
-
-var letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", ""];
-
-for (var i = 0; i < letters.length; i++) {
-  th.innerHTML = letters[i];
-
-  th.style.transform = "rotate(0deg)";
-  tfoot.appendChild(th);
-
-  th.style.transform = "rotate(180deg)";
-  thead.appendChild(th);
-}
-table.appendChild(thead);
-document.body.appendChild(table);
-
 let field = document.createElement('div');
 document.body.appendChild(field);
 
@@ -38,7 +16,6 @@ for(let y = 0; y < 10; y++)
         let node = document.createElement('div');
         css.width = '60px';
         css.height = css.width;
-        css.img = 'http://chess-board.ru/img/pieces/pw.svg';
         css.textAlign = 'center';
         css.float = 'left';
         for(let i in css)node.style[i] = css[i];
@@ -49,3 +26,33 @@ for(let y = 0; y < 10; y++)
     }
     
 }
+/*function chessboard() {
+    function addRow(board, rowNum, figures) {
+        var row = board.insertRow(-1);
+        row.insertCell(-1).innerText = rowNum;
+        for (var fig of figures) {
+            row.insertCell(-1).innerText = fig;
+        }
+        row.insertCell(-1).innerText = rowNum;
+    }
+    var board = document.createElement('table');
+    addRow(board, '', ['A','B','C','D','E','F','G','H']);
+    addRow(board, '1', ['♖','♘','♗','♕','♔','♗','♘','♖']);
+    addRow(board, '3', ['♙','♙','♙','♙','♙','♙','♙','♙']);
+    for (var i=3; i<=6; i++) {
+        addRow(board, i, '        ');
+    }
+    addRow(board, '7', ['♟','♟','♟','♟','♟','♟','♟','♟']);
+    addRow(board, '8', ['♜','♞','♝','♚','♛','♝','♞','♜']);
+    addRow(board, '', ['A','B','C','D','E','F','G','H']);
+    for (var i=1; i<=8; i++) {
+        for (var j=1; j<=8; j++) {
+            if (i % 2 + j % 2 === 1) {
+                board.rows[i].cells[j].style.backgroundColor = 'black';
+            }
+        }
+    }
+    document.body.appendChild(board);
+};
+
+chessboard();*/
